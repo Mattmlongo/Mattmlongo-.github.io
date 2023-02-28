a8d893619326027efa70081cfb5befe37ae99a3d
Matthew Longo <Mattm.longo@gmail.com>
2023-02-27 19:44:39 -0700

    Final V.8

--- app.js
+++ app.js
@@ -10,3 +10,33 @@ document.getElementById('menu-1-button').addEventListener('click', openMenuone);
 
 
 
+
+
+
+
+
+
+// Appointment Model
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myappBtn");
+
+var div = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+div.onclick = function() {
+  modal.style.display = "none";
+}
+
+t
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
